58f54b7d3c74e660676179626620d221672e84f4
Kris <you@example.com>
2022-03-11 09:56:57 -0600

    Additional design changes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,9 +6,9 @@ const {jokes, getJokes} = useApi();
 
 <template>
   <div class="flex flex-row items-center justify-center">
-    <button  @click="getJokes" class="px-4 py-6 mt-8 bg-red-600 rounded-xl hover:bg-red-700 hover:text-red-300">
-      CHUCK NORRIS!
+    <button  @click="getJokes" class="px-6 py-6 mt-12 bg-red-600 rounded-xl hover:bg-red-700 hover:text-red-300">
+      JOKE TIME!
       </button>
   </div>
-  <p class="flex flex-row items-center justify-center">{{ jokes }}</p>
+  <p class="flex flex-row items-center justify-center mt-12 text-lg">{{ jokes }}</p>
 </template>
